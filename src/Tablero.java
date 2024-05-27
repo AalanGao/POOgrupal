7af5fe8ee92a61762fe0f4edd50fdef8aa97fd46
tentu <vigiluade@gmail.com>
2024-05-27 11:01:20 -0300

    Creación de casillas en tablero

--- src/Tablero.java
+++ src/Tablero.java
@@ -7,7 +7,20 @@ public class Tablero {
             casillas[i] = new Casilla(i, TipoCasilla.COMUN);
         }
         this.casillas[5] = new Oca(5, TipoCasilla.OCA);
+        int[] array= {5,9,14,18,23,27,32,36,41,45,50,54,59};
+     for (int i = 0; i < array.length; i++) {
+            this.casillas[array[i]] = new Oca(array[i], TipoCasilla.OCA);
+        }
+     this.casillas[6] = new DestinoDirecto(6, TipoCasilla.PUENTE, 19);
+     this.casillas[12] = new DestinoDirecto(12, TipoCasilla.PUENTE, 19);
+     this.casillas[19] = new CambioTurno(19, TipoCasilla.POSADA,-1);
+     this.casillas[42] = new DestinoDirecto(42, TipoCasilla.PUENTE, 30);
+     this.casillas[26] = new CambioTurno(56, TipoCasilla.VOLVERLANZARDADOS,1);
+     this.casillas[53] = new CambioTurno(53, TipoCasilla.VOLVERLANZARDADOS,1);
+     this.casillas[58] = new DestinoDirecto(58, TipoCasilla.CALAVERA, 1);
+
     }
+
 
     public Casilla[] getCasillas() {
         return casillas;
